7cebb4a72e79a27bdbc99bb39089827989dbc251
Curtis Hammons <curtishammons@gmail.com>
2020-08-11 14:39:07 -0700

    minor corrections for resubmission

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'cjhammons.us.auth0.com', // the auth0 domain prefix
+    url: 'cjhammons.us', // the auth0 domain prefix
     audience: 'https://coffee-shop', // the audience set for the auth0 app
     clientId: '4wudeiPk17FqWjhetF7l8a0KDmT0oTPZ', // the client id generated for the auth0 app
     callbackURL: 'http://127.0.0.1:8080', // the base url of the running ionic application. 
